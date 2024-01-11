e7afe4160ddfe279fd62c9986ea18e96f139b172
LAPikachu <131661768+LAPikachu@users.noreply.github.com>
2024-01-11 23:46:47 +0100

    doijng some work

--- make_crystal.py
+++ make_crystal.py
@@ -1,9 +1,23 @@
 import matplotlib.pyplot as plt
 from ase.visualize.plot import plot_atoms
-from ase.lattice.cubic import FaceCenteredCubic
+from ase.lattice.cubic import FaceCenteredCubic, BodyCenteredCubic, SimpleCubic
 from ase.visualize import view
-slab = FaceCenteredCubic('Cu', size=(2, 2, 2))
-fig, ax = plt.subplots()
-plot_atoms(slab, ax, radii=0.3)
-fig.savefig("ase_slab.png")
-view(slab)
+
+sc_cell = SimpleCubic('Cu', latticeconstant=1 ,size=(2, 2, 2))
+fcc_cell = FaceCenteredCubic('Cu', latticeconstant=1 ,size=(2, 2, 2))
+bcc_cell = BodyCenteredCubic('Cu', latticeconstant=1, size=(2,2,2)) 
+
+fig, axs = plt.subplots(1, 3, figsize=(10, 5))
+
+plot_atoms(sc_cell, ax=axs[0], radii=radius)
+plot_atoms(fcc_cell, ax=axs[1], radii=radius)
+plot_atoms(bcc_cell, ax=axs[2], radii=radius)
+
+
+
+axs[0].set_title('Simple Cubic Lattice')
+axs[1].set_title('Face-Centered Cubic Lattice')
+axs[2].set_title('Body-Centered Cubic Lattice')
+fig.savefig("crystal_lattices.png")
+
+#view(fcc_cell)
