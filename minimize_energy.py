b1c506cabfab1f5faa30b4b3bdf9265e58109f04
LAPikachu <131661768+LAPikachu@users.noreply.github.com>
2024-01-14 23:38:40 +0100

    computed ernergies and fiting curves

--- minimize_energy.py
+++ minimize_energy.py
@@ -1,39 +1,63 @@
 import numpy as np
+import matplotlib.pyplot as plt 
+from scipy.optimize import curve_fit 
 from ase.calculators.emt import EMT
 from ase.lattice.cubic import FaceCenteredCubic, BodyCenteredCubic, SimpleCubic
 from ase.io import Trajectory, read
-from ase.eos import EquationOfState
 
-L_sc = 2.56
-L_fcc = 3.62
-L_bcc = 2.96
+#dictionary of initial values for lattice constants (all in Angstrom)
+L_init_values = {'sc' : 2.56,
+                 'fcc' : 3.62,
+                 'bcc' : 2.96}
 
-sc_cell = SimpleCubic('Cu', latticeconstant=L_sc ,size=(2, 2, 2))
-fcc_cell = FaceCenteredCubic('Cu', latticeconstant=L_fcc, size=(2, 2, 2))
-bcc_cell = BodyCenteredCubic('Cu', latticeconstant=L_bcc, size=(2,2,2)) 
+#traj_sc = Trajectory('sc.traj', 'w')
 
-print(sc_cell.cell[0,0])
+def create_cubic_lattice(lattice_type, L):
+    switch_dict = {'sc' : SimpleCubic('Cu', latticeconstant=L, size=(2,2,2)),
+                   'bcc' : BodyCenteredCubic('Cu', latticeconstant=L, size=(2,2,2)),
+                   'fcc' : FaceCenteredCubic('Cu', latticeconstant=L, size=(2,2,2))}  
+    return switch_dict[lattice_type]
 
-traj_sc = Trajectory('sc.traj', 'w')
+def calculate_latticeconst_epot_traj(lattice_type, L_init):
+    traj_file = Trajectory(f'{lattice_type}.traj', 'w')
+    lattice_constants_list = []
+    for L in np.linspace(L_init - 0.5 , L_init + 1, 100):
+        lattice_constants_list.append(L)
+        data = create_cubic_lattice(lattice_type, L)
+        data.calc = EMT()
+        data.get_potential_energy()
+        traj_file.write(data)
+    return lattice_constants_list
 
-eps = 0.01
-for L in L_sc * np.linspace(1 - eps, 1 + eps, 10):
-    sc_cell = SimpleCubic('Cu', latticeconstant=L ,size=(2, 2, 2))
-    sc_cell.calc = EMT()
-    epot = sc_cell.get_potential_energy()
-    print(epot)
-    traj_sc.write(sc_cell)
+if __name__ =='__main__':
+    
+    dict = {}
+    # loop through crystal structures and lattice constants
+    for lattice_type in L_init_values.keys():
+        L_init = L_init_values[lattice_type]
+        lattice_constants_list = calculate_latticeconst_epot_traj(lattice_type, L_init)
+        configs = read(f'{lattice_type}.traj@0:', 'r') #@0: means go though all the saved configurations
+        epots_list = [data.get_potential_energy() for data in configs]
+        dict[lattice_type] = [lattice_constants_list, epots_list]
+    #plot the data
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Lattice constant in Angstrom')
+    ax.set_ylabel('Potential energy in eV')
+    for lattice_type in dict.keys():
+        x,y = dict[lattice_type]
+        ax.plot(x, y, 'o' , label = f'{lattice_type} lattice')
+    ax.legend()
+    plt.show()
+    #fit it to a 2nd order polynomial
 
-configs = read('sc.traj@0:')
-'''
-in the next list comp I forgot () after .get_potential_energy()
-... no wonder nothing worked
-'''
-energies = [point.get_potential_energy() for point in configs] 
+    def W(L, p_0, p_1, p_2):
+        return p_0 + p_1* L + p_2 * L**2
+    
+    for lattice_type in L_init_values.keys():
+        x,y = dict[lattice_type]
+        popt, pcov = curve_fit(W, x, y) 
 
-with open("config_out", 'w') as file:
-        for entry in energies:
-            file.write(entry)
-
+        ax.plot(x, [W(x_i, *popt) for x_i in x],
+                '--', label = f'fit {lattice_type} lattice')
 
 
