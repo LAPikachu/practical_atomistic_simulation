72869f18b37bd7ec2798c2ad82fd0b4ead7aec2a
LAPikachu <131661768+LAPikachu@users.noreply.github.com>
2024-01-15 23:06:01 +0100

    finished task 3

--- minimize_energy.py
+++ minimize_energy.py
@@ -6,12 +6,13 @@ from ase.calculators.emt import EMT
 from ase.lattice.cubic import FaceCenteredCubic, BodyCenteredCubic, SimpleCubic
 from ase.io import Trajectory, read
 
+#List of crystal lattice types
+lattice_types = ['sc', 'fcc', 'bcc']
+
 #dictionary of initial values for lattice constants (all in Angstrom)
 L_init_values = {'sc' : 2.56,
                  'fcc' : 3.62,
                  'bcc' : 2.96}
-
-#traj_sc = Trajectory('sc.traj', 'w')
 
 def create_cubic_lattice(lattice_type, L):
     switch_dict = {'sc' : SimpleCubic('Cu', latticeconstant=L, size=(2,2,2)),
@@ -23,38 +24,47 @@ def calculate_latticeconst_epot_traj(lattice_type, L_init):
     traj_file = Trajectory(f'{lattice_type}.traj', 'w')
     lattice_constants_list = []
     for L in np.linspace(L_init - 0.5 , L_init + 1, 100):
-        lattice_constants_list.append(L)
+        lattice_constants_list.append(L) # append current lattice constant
         data = create_cubic_lattice(lattice_type, L)
         data.calc = EMT()
         data.get_potential_energy()
         traj_file.write(data)
     return lattice_constants_list
 
-def write_data_to_json():
+def write_data_dict(lattice_type ,lattice_constants_list):
     data_dict = {}
-    # loop through crystal structures and lattice constants
-    for lattice_type in L_init_values.keys():
-        L_init = L_init_values[lattice_type]
-        lattice_constants_list = calculate_latticeconst_epot_traj(lattice_type, L_init)
-        configs = read(f'{lattice_type}.traj@0:', 'r') #@0:  go though all the saved configurations
-        epots_list = [data.get_potential_energy() for data in configs]
-        data_dict[lattice_type] = [lattice_constants_list, epots_list]
-    with open('epot_latticeconst', 'w') as data:
+    L_init = L_init_values[lattice_type]
+    calculate_latticeconst_epot_traj(lattice_type, L_init)
+    configs = read(f'{lattice_type}.traj@0:', 'r') #@0:  go though all the saved configurations
+    epots_list = [data.get_potential_energy() for data in configs] #list potential energies
+    data_dict[lattice_type] = [lattice_constants_list, epots_list] #array pairing epot w/ corresponding latticeconst
+    return data_dict[lattice_type]
+
+#save data from dicts to permanent file
+def save_data_to_json(data_dict, filename):
+    with open(filename, 'w') as data:
         json.dump(data_dict, data)
-    
-    
 
 if __name__ =='__main__':
-    #write_data_to_json()
+    '''
+    data_dict = {}
+    for lattice_type in lattice_types:
+        L_init = L_init_values[lattice_type]
+        lattice_constants_list = calculate_latticeconst_epot_traj(lattice_type, L_init)
+        data_dict[lattice_type] = write_data_dict(lattice_type, lattice_constants_list)
+    save_data_to_json(data_dict, 'epot_latticeconst')
+    '''
+
     with open('epot_latticeconst', 'r') as data:
        data_dict = json.load(data)
     #plot the data
     fig, ax = plt.subplots()
     ax.set_xlabel('Lattice constant in Angstrom')
     ax.set_ylabel('Potential energy in eV')
-    for lattice_type in data_dict.keys():
+    # for plotting
+    for lattice_type, marker_type in zip(lattice_types, ['1','<','x']):
         x,y = data_dict[lattice_type]
-        ax.plot(x, y, 'o' , label = f'{lattice_type} lattice')
+        ax.plot(x, y, marker_type , label = f'{lattice_type} lattice')
     ax.legend()
     #fit it to a 2nd order polynomial
 
@@ -62,21 +72,27 @@ if __name__ =='__main__':
         return p_0 + p_1* L + p_2 * L**2
 
     fitting_params = {}
-    for lattice_type in data_dict.keys():
+    for lattice_type in lattice_types:
         x,y = data_dict[lattice_type]#[:60][:60] could adjust over which values to fit
         popt, pcov = curve_fit(W_2nd, x, y) 
         ax.plot(x, [W_2nd(x_i, *popt) for x_i in x],
                 '--', label = f'2nd order fit {lattice_type} lattice')
         fitting_params[lattice_type] = popt
+    ax.legend()
+    fig.savefig('epot_lattice_plot')
     '''
     fit is not really optimal
     2nd ord poly not ideal for theses kinds of curvature?
     '''
     roots_dict = {}
-    for lattice_type in  data_dict.keys():
+    for lattice_type in  lattice_types:
         p_1, p_2 = fitting_params[lattice_type][1:] # not from 0 since p_0 sits at 0
         def dW_2nd_zero(L):
             return p_1 + 2 * p_2 * L
         L_init = L_init_values[lattice_type]
-        roots_dict[lattice_type] = root(dW_2nd_zero, L_init).x
-    print(roots_dict)
+        roots_dict[lattice_type] = root(dW_2nd_zero, L_init).x[0]
+    save_data_to_json(roots_dict, 'lattice_consts')
+    print("Lattice constants: \n"
+          "Sc {L_sc} \nbcc {L_bcc}"
+          "\nfcc {L_fcc} ".format(L_sc=roots_dict['sc'],
+                                  L_bcc=roots_dict['bcc'], L_fcc=roots_dict['fcc']))
