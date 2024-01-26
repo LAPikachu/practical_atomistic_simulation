4a0f11c39ab744b0f087ee802dc3b090c7686d6c
LAPikachu <131661768+LAPikachu@users.noreply.github.com>
2024-01-26 19:42:56 +0100

    added clean up for exportdata

--- deform_cnt.py
+++ deform_cnt.py
@@ -10,9 +10,12 @@ from ase.optimize import FIRE
 from ase.md.verlet import VelocityVerlet
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase import units
+import os
+from pathlib import Path
 #from tqdm import tqdm
 
 directory = 'data_CNT_sim/'
+data_directory = 'data_CNT_sim/nve_deform/'
 cnt = nanotube(5, 5, symbol='C', length=1)
 cnt.calc = EMT()
 
@@ -24,45 +27,62 @@ observing_traj = Trajectory(f'{directory}cnt.traj', 'w')
 
 def deform_in_z(cnt, eps):
     cnt_array = cnt.cell[:]
-    cntcnt_array_new = np.dot(cnt_array, (np.eye(3) + np.array([[0,0,0],[0,0,0],[0,0,eps]])))
-    cnt.cell = cnt_array_new
+    cnt_array_new = cnt_array + np.dot(cnt_array_original, (np.array([[0,0,0],[0,0,0],[0,0,eps]])))
+    cnt.set_cell(cnt_array_new, scale_atoms=True)
 
-
-
+def run_simulation(loop_number, step_number, timestep, eps):
+    for i in range(loop_number):
+            deform_in_z(cnt, eps)
+            MaxwellBoltzmannDistribution(cnt, temperature_K=0)
+            dyn.run(steps=step_number)
+            data['time'].append(i*step_number*timestep)
+            data['temperatur'].append(cnt.get_temperature()/len(cnt))
+            data['epot'].append(cnt.get_potential_energy()/len(cnt))
+            data['ekin'].append(cnt.get_kinetic_energy()/len(cnt))
+            data['etot'].append(cnt.get_total_energy()/len(cnt))
+            print(f'Rep {i} of {loop_number}')
+            if i%10 == 0:
+                lammpsdata.write_lammps_data(f'{data_directory}cnt_{i}.dump', cnt) # dump to lammps dump so ovito can show it
+            with open(f'{directory}data.json', 'w') as fp:
+                json.dump(data, fp)
 
 if __name__ == '__main__':
+    for f in Path(f'{data_directory}').glob('*.dump'): #clean up output path for dump files
+        try:
+              f.unlink()
+        except:
+             print('no files in data dir')
 
-    MaxwellBoltzmannDistribution(cnt, temperature_K=0)
-    dyn = VelocityVerlet(cnt, timestep=0.1*units.fs)
-    eps = 0.10
-    data = {'time':[],'temperatur':[], 'epot':[],'ekin':[]}
-    
-    dyn.run(100)
 
-    for i in range(10):
-        data['time'].append(i)
-        deform_in_z(cnt, eps)
-        print(cnt.cell[:])
-        dyn.run(10)
-        data['temperatur'].append(cnt.get_temperature())
-        data['epot'].append(cnt.get_potential_energy())
-        data['ekin'].append(cnt.get_kinetic_energy())
-        print(f'Rep {i} of 100')
-        lammpsdata.write_lammps_data(f'{directory}cnt.dump', cnt) # dump to lammps dump so ovito can show it
+    strain_rate = 4000
+    #strain_rate = eps/(timestep * step_number*10**-12)
+    timestep = 1
+    step_number = 10 
+    loop_number = 200
+    eps = strain_rate * timestep * step_number*10**-12
+    #eps = 0.001
+    total_strain = 1+eps*loop_number 
+    dyn = VelocityVerlet(cnt, timestep=timestep*units.fs)
+    data = {'time':[],'temperatur':[], 'epot':[],'ekin':[], 'etot': []}
+    cnt_array_original = cnt.cell[:]
+    MaxwellBoltzmannDistribution(cnt, temperature_K=300)
 
-    with open(f'{directory}data.json', 'w') as fp:
-        json.dump(data, fp)
-    
+
+    run_simulation(loop_number, step_number, timestep, eps) 
+
     with open(f'{directory}data.json', 'r') as fp:
         data = json.load(fp)
     
     fig, ax = plt.subplots()
-    ax.plot(data['time'], data['temperatur'], label = 'temperatur')
-    ax.plot(data['time'], data['ekin'], label = 'ekin')
-    ax.plot(data['time'], data['epot'], label = 'epot')
+    ax.set_title('strain rate {a:e} $s^-1$, total strain {b} '.format(a=strain_rate, b=total_strain))
+    ax.plot(data['time'], data['temperatur'],'s', label = 'temperatur')
+    ax.plot(data['time'], data['ekin'],'o', label = 'ekin')
+    ax.plot(data['time'], data['epot'],'x', label = 'epot')
+    ax.plot(data['time'], data['etot'], '.', label = 'etot')
     ax.legend()
     plt.show()
-    
+    print('done')
+   #0.01, 0.001 
 
 
         
