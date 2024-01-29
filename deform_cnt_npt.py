914ea36464ef9634ca974199b0676020de8f2210
LAPikachu <131661768+LAPikachu@users.noreply.github.com>
2024-01-29 09:01:25 +0100

    data now also catches data within one run step

--- deform_cnt_npt.py
+++ deform_cnt_npt.py
@@ -7,7 +7,7 @@ from ase.calculators.emt import EMT
 from ase.build import nanotube
 from ase.io import Trajectory, read, write, lammpsdata
 from ase.optimize import FIRE
-from ase.md.verlet import VelocityVerlet
+from ase.md.langevin import Langevin
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase import units
 import os
@@ -15,8 +15,8 @@ from pathlib import Path
 #from tqdm import tqdm
 
 directory = 'data_CNT_sim/'
-data_directory = 'data_CNT_sim/nve_deform/'
-cnt = nanotube(5, 5, symbol='C', length=1, bond=1.42)
+data_directory = 'data_CNT_sim/nvt_deform/'
+cnt = nanotube(5, 5, symbol='C', length=1)
 cnt.calc = EMT()
 
 start_traj = Trajectory(f'{directory}cnt_initial.traj', 'w')
@@ -37,26 +37,22 @@ def printenergy(atoms=cnt):
      print(f'epot: {epot}, ekin: {ekin}, temperaure: {temp}')
 
 def run_simulation(loop_number, step_number, timestep, eps):
-    dyn.attach(printenergy)
-    dyn.run(100) #eqiliberate befor running
     for i in range(loop_number):
-            loop_time = i*step_number
             deform_in_z(cnt, eps)
+            #MaxwellBoltzmannDistribution(cnt, temperature_K=300)
             #dyn.attach(printenergy)
-            for step in range(step_number): 
-                dyn.run(1)
-                if (loop_time+step)%1 == 0:
-                    data['time'].append((loop_time+step)*timestep)
-                    data['temperatur'].append(cnt.get_temperature())
-                    data['epot'].append(cnt.get_potential_energy())
-                    data['ekin'].append(cnt.get_kinetic_energy())
-                    data['etot'].append(cnt.get_total_energy())
-                
+            dyn.run(steps=step_number)
+            data['time'].append(i*step_number*timestep)
+            data['temperatur'].append(cnt.get_temperature())
+            data['epot'].append(cnt.get_potential_energy())
+            data['ekin'].append(cnt.get_kinetic_energy())
+            data['etot'].append(cnt.get_total_energy())
+            
             print(f'Rep {i+1} of {loop_number}')
             if i%10 == 0:
                 lammpsdata.write_lammps_data(f'{data_directory}cnt_{i}.dump', cnt) # dump to lammps dump so ovito can show it
-    with open(f'{data_directory}data.json', 'w') as fp:
-        json.dump(data, fp)
+                with open(f'{data_directory}data.json', 'w') as fp:
+                    json.dump(data, fp)
 
 if __name__ == '__main__':
     for f in Path(f'{data_directory}').glob('*.dump'): #clean up output path for dump files
@@ -66,19 +62,20 @@ if __name__ == '__main__':
              print('no files in data dir')
 
     
-    strain_rate = 10**8 #in s^-1 
+    strain_rate = 8 * 10**8
     #strain_rate = eps/(timestep * step_number*10**-12)
-    timestep = 0.1 #in fs
-    step_number = 100
-    loop_number = 100
+    timestep = 0.1 
+    step_number = 10
+    loop_number = 500
     eps = strain_rate * timestep * step_number*10**-12
     #eps = 0.001
     total_strain = eps*loop_number 
-    dyn = VelocityVerlet(cnt, timestep=timestep*units.fs)
+    dyn = Langevin(cnt, timestep=timestep*units.fs, temperature_K= 300, friction=0.01/units.fs)
     data = {'time':[],'temperatur':[], 'epot':[],'ekin':[], 'etot': []}
     cnt_array_original = cnt.cell[:]
-    MaxwellBoltzmannDistribution(cnt, temperature_K=0)
-    run_simulation(loop_number, step_number, timestep, eps) 
+    #dyn.attach(printenergy)
+    #dyn.run(600)
+    #run_simulation(loop_number, step_number, timestep, eps) 
     with open(f'{data_directory}data.json', 'r') as fp:
         data = json.load(fp)
     
@@ -87,8 +84,8 @@ if __name__ == '__main__':
     ax.set_xlabel('time in fs')
     ax.set_ylabel('Epot, Ekin, Etot in eV, T in K')
     #ax.plot(data['time'], data['temperatur'],'s', label = 'temperatur')
-    ax.plot(data['time'], data['ekin'],'.', label = 'ekin')
-    ax.plot(data['time'], data['epot'],'.', label = 'epot')
+    ax.plot(data['time'], data['ekin'],'o', label = 'ekin')
+    ax.plot(data['time'], data['epot'],'x', label = 'epot')
     ax.plot(data['time'], data['etot'], '.', label = 'etot')
     ax.legend()
     plt.show()
